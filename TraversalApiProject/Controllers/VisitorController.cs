ae2cfee70fd9ccff69abaeef837045dd949d46c8
Egemen Ağustos <egemenagustos351903@gmail.com>
2023-01-31 19:03:58 +0300

    Add project files.

---
+++ TraversalApiProject/Controllers/VisitorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitorController : ControllerBase
+    {
+    }
+}
