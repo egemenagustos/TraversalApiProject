8d0e2c07d4bf036b0365bce5576094d7bb6ef280
Egemen Ağustos <egemenagustos351903@gmail.com>
2023-01-31 19:49:28 +0300

    .

--- TraversalApiProject/Controllers/VisitorController.cs
+++ TraversalApiProject/Controllers/VisitorController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TraversalApiProject.DAL.Context;
+using TraversalApiProject.DAL.Entity;
 
 namespace TraversalApiProject.Controllers
 {
@@ -7,5 +10,85 @@ namespace TraversalApiProject.Controllers
     [ApiController]
     public class VisitorController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult VisitorList()
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.ToList();
+                return Ok(values);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult VisitorAdd(Visitor visitor)
+        {
+            using (var context = new VisitorContext())
+            {
+                context.Visitors.Add(visitor);
+                context.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult VisitorGet(int id)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(id);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(values);
+                }
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteVisitor(int id)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(id);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    context.Remove(values);
+                    context.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateVisitor(Visitor visitor)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Find<Visitor>(visitor.VisitorID);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    values.City = visitor.City;
+                    values.Name = visitor.Name;
+                    values.Mail = visitor.Mail;
+                    values.Surname = visitor.Surname;
+                    values.Country = visitor.Country;
+                    context.Update(values);
+                    context.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
     }
 }
---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entity;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=EGEMEN\\SQLEXPRESS; initial catalog=TraversalDbApi; integrated security=true;");
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
